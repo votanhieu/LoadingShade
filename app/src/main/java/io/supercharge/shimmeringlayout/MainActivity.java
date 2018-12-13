bbc63755d49d02ad1a6fcaccf09045fa91e99ec9
votanhieu <votanhieu.2@gmail.com>
2018-12-13 23:44:19 +0700

    First commit

---
+++ app/src/main/java/io/supercharge/shimmeringlayout/MainActivity.java
@@ -0,0 +1,18 @@
+package io.supercharge.shimmeringlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.supercharge.shimmerlayout.ShimmerLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ShimmerLayout shimmerLayout = findViewById(R.id.shimmer_layout);
+        shimmerLayout.startShimmerAnimation();
+    }
+}
